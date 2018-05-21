53a1ff150137aedfa372bb6230fbe3149e086721
Annisa Dwi Oktavianita <annisadwioktavianita@gmail.com>
2018-05-21 14:22:25 +0700

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mangoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb')
+
 var app = express();
 
 app.use(bodyParser.json())
@@ -25,6 +27,22 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
     }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((e) => {
         res.status(400).send(e);
     })
 })
